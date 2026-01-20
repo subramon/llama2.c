19a964269992c7c68cc69d4e426fb916f9028a05
Ramesh Subramonian <subramonian@gmail.com>
2026-01-20 11:14:35 -0800

    ok

--- run.c
+++ run.c
@@ -201,7 +201,7 @@ forward(
     // STRANGE: Slows it down for gcc but not for ISPC. Puzzling...
     // However, in one simple case, speedup was only 1.5x
     // I don't think there is enough work to justify overhead of omp
-#pragma omp parallel for schedule(static, 1)
+// #pragma omp parallel for schedule(static, 1)
     for ( int h = 0; h < p->n_heads; h++) {
       // get the query vector for this head
       const float* const q_h = mcr_2d_to_1d(s->q, h, ispc_head_size); 
